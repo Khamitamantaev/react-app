259fdc28150bd30e93cbb61fa06023db476e64fb
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-12-07 21:28:28 +0600

    transfer todolist elements to props

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import TodoListItem from './todo-list-item'
-const TodoList = () => {  
+const TodoList = ({ todos }) => {
+
+    const elements = todos.map((item) => {
+        return (
+        <li key={item.id}>
+            <TodoListItem {...item} />
+        </li>)
+    })
     return (
-      <ul>
-        <li><TodoListItem label="Drink Coffee" /></li>
-        <li><TodoListItem 
-            label="Build Awesome React App"
-            important
-        / ></li>
-      </ul>
+        <ul>
+            {elements}
+        </ul>
     )
-  }
+}
 
 export default TodoList
--- src/index.js
+++ src/index.js
@@ -8,12 +8,28 @@ const root = createRoot(container);
 
 
 const App = () => {
+  const todoData = [
+    {
+      id: 1,
+      label: 'Drink Coffee',
+      important: false
+    },
+    { id: 2,
+      label: 'Make Awesome App',
+      important: true
+    },
+    {
+      id: 3,
+      label: 'Deploy to Vercel',
+      important: true
+    }
+  ]
 
   return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData}/>
     </div>
   )
 }
