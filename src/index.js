ac75e260d020352ae61932be703483ce639672de
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-12-08 15:37:49 +0600

    indexjs for default import

--- src/index.js
+++ src/index.js
@@ -1,43 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
-import TodoList from './components/todo-list/todo-list';
-import AppHeader from './components/app-header/app-header';
-import SearchPanel from './components/search-panel/search-panel';
-import ItemStatusFilter from './components/item-status-filter/item-status-filter';
-import './index.css'
+import App from './components/app';
 const container = document.getElementById('root');
 const root = createRoot(container);
-
-
-const App = () => {
-  const todoData = [
-    {
-      id: 1,
-      label: 'Drink Coffee',
-      important: false
-    },
-    { id: 2,
-      label: 'Make Awesome App',
-      important: true
-    },
-    {
-      id: 3,
-      label: 'Deploy to Vercel',
-      important: true
-    }
-  ]
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  )
-}
 
 root.render(<App />);
