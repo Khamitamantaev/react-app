aee3cd9a351f770b4229d3cc8a60b3e20dede05b
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-12-22 22:17:23 +0600

    add item to array

--- src/components/add-item/add-item-form.js
+++ src/components/add-item/add-item-form.js
@@ -4,15 +4,37 @@ import "./add-item-form.css"
 
 
 export default class AddItemForm extends Component {
-    
+
+    state = {
+        label: ''
+    }
+
+    handleInputChange = (e) => {
+       this.setState({
+            label: e.target.value
+       })
+    }
+
+    onSubmit = (e) =>  {
+        e.preventDefault() // Чтобы не было перезагрузки страницы после отправки формы
+        this.props.onAdded(this.state.label)
+    }
 
     render() {
         return (
-            <div className="add-item-form">
-                <button className="btn btn-outline-secondary" onClick={() => this.props.onAdded('dasd')}>
+            <form className="add-item-form d-flex"
+                  onSubmit={this.onSubmit}
+            >
+                <input
+                    type="text"
+                    className="form-control"
+                    onChange={this.handleInputChange}
+                    placeholder="What needs to be done"
+                />
+                <button className="btn btn-outline-secondary">
                     Add Item
                 </button>
-            </div>
+            </form>
         )
     }
 }
